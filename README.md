d684fd6630480fe3f682a3a22234003a66a4c7a8
carlos <orellano428@gmail.com>
2024-01-15 23:55:09 -0500

    push final

--- README.md
+++ README.md
@@ -23,6 +23,12 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
+5. Recontruir la base de datos GET
+
+```
+http://localhost:3000/api/v2/seed
+```
+
 ## Stack
 
 - MongoDB
---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,12 @@
+import { IsOptional, Min, IsPositive, IsNumber } from 'class-validator';
+
+export class PaginationDto {
+  @IsOptional()
+  @IsPositive()
+  @IsNumber()
+  @Min(1)
+  limit?: number;
+  @IsOptional()
+  @IsPositive()
+  offset?: number;
+}
